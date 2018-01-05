6a15255e130db16d3a9c1addaa29fe3839de8344
Tarasov V <vovatarasov6991@gmail.com>
2018-01-05 13:44:21 +0200

    fix mapbox e uncaught error

--- src/app/map-box/map-box.component.ts
+++ src/app/map-box/map-box.component.ts
@@ -37,7 +37,7 @@ export class MapBoxComponent implements OnInit {
   }
 
   buildMap() {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiYm9nZGFubW9sb2RldHMiLCJhIjoiY2pjMG9kZ3NjMDNhazJ4cXltNWdhYXh0diJ9.RbZ5rCF0N3-n5GKfGyrI3w';
+    ( mapboxgl as any).accessToken = 'pk.eyJ1IjoiYm9nZGFubW9sb2RldHMiLCJhIjoiY2pjMG9kZ3NjMDNhazJ4cXltNWdhYXh0diJ9.RbZ5rCF0N3-n5GKfGyrI3w';
     this.map = new mapboxgl.Map({
       container: 'map', // container id
       style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
