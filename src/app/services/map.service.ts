9c9a3d04c57be9f1789d5afe352b84f40018d4af
Tarasov V <vovatarasov6991@gmail.com>
2018-01-11 18:18:37 +0200

    add polygon draw on marker click

--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -1,12 +1,14 @@
 import {Injectable, ElementRef} from '@angular/core';
 import {Event} from '../event';
+import {EventsService} from '../services/events.service';
 import {Observable} from 'rxjs/Observable';
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
-import {isUndefined} from 'util';
+import {isNumber, isUndefined} from 'util';
+import {ViewComponent} from 'ngx-openlayers';
 
 @Injectable()
 export class MapService {
-  private map:ElementRef
+  // private map:ElementRef
   private zoomSource = new BehaviorSubject<number>(5);
   currentZoom = this.zoomSource.asObservable();
   private longSource = new BehaviorSubject<number>(5);
@@ -14,13 +16,14 @@ export class MapService {
   private latSource = new BehaviorSubject<number>(45);
   currentLat = this.latSource.asObservable();
   private visibleSource = new BehaviorSubject<boolean>(false);
-  currentVisible= this.visibleSource.asObservable();
+  currentVisible = this.visibleSource.asObservable();
   private eventSource = new BehaviorSubject<Event>(null);
-  currentEvent= this.eventSource.asObservable();
-  // public long = new BehaviorSubject<number>(5);
-  // public lat = new BehaviorSubject<number>(45);
+  currentEvent = this.eventSource.asObservable();
+  // private viewSource = new BehaviorSubject<ViewComponent>(null);
+  // currentView = this.viewSource.asObservable();
 
-  constructor() {
+
+  constructor(private eventService: EventsService) {
   }
 
   changeInitMap(z: number, ln: number, lt: number, v: boolean, e: Event) {
@@ -29,30 +32,33 @@ export class MapService {
     this.latSource.next(lt);
     this.visibleSource.next(v);
     this.eventSource.next(e);
+    // this.viewSource.next()
   }
-  public GetMap(){
-    return this.map;
-  }
-  public SetMap(map:ElementRef){
-    this.map = map;
-  }
+
 
   public OnCardClick(event: Event, v: boolean) {
     this.changeInitMap(7, event.event_lon, event.event_lat, v, event);
   }
 
-  public OnPointClick(evn: any) {
-    // get map object from click event
+  public OnPointClick(evn: any, events: Event[]) {
+
     const mp = evn.map;
-    console.log(evn);
-    console.log(evn.map);
     const clicked_feature = mp.forEachFeatureAtPixel(evn.pixel, function (feature, layer) {
-      return feature;
+
+      if (!isUndefined(feature.getId())) {
+        return feature;
+      }
     });
+
     if (!isUndefined(clicked_feature)) {
+      const e = events.find(function (event: Event, index: number, array: Event[]) {
+        return event.id === clicked_feature.getId();
+      });
+      console.log(e);
+      // const e = clicked_feature.getId('id');
       mp.getView().setZoom(7);
       mp.getView().setCenter(clicked_feature.getGeometry().getCoordinates());
-
+      this.eventSource.next(e);
     }
 
 
