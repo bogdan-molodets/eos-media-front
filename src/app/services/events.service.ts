96ae52f1aee34a7117e64f043910584e341c9779
bogdan.molodets <bogdan.molodets@eosda.com>
2018-03-03 10:01:29 +0200

    change http to https in pagination by id query

--- src/app/services/events.service.ts
+++ src/app/services/events.service.ts
@@ -20,7 +20,7 @@ export class EventsService {
   private tweets_url = 'https://gruz-test-blog.herokuapp.com/api/tweet_view/';
   private types_url = 'https://media-test-service.herokuapp.com/event-types/';
   private filters_url = 'https://media-test-service.herokuapp.com/events/many_filter/';
-  private page_url = 'http://media-test-service.herokuapp.com/event-id/';
+  private page_url = 'https://media-test-service.herokuapp.com/event-id/';
 
   /*
     stateFire = new BehaviorSubject<boolean>(true);
