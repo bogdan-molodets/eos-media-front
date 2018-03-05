6dd01c50fd423cd9da6d00a0b9de7bb3ace18b43
bogdan.molodets <bogdan.molodets@eosda.com>
2018-03-05 17:00:54 +0200

    fix bug with twitter_link

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -24,8 +24,8 @@ export class CardComponent implements OnInit {
 
   private url;
   private accessToken = 'pk.eyJ1IjoiYm9nZGFubW9sb2RldHMiLCJhIjoiY2pjMG9kZ3NjMDNhazJ4cXltNWdhYXh0diJ9.RbZ5rCF0N3-n5GKfGyrI3w';
-  private twitter_link;
-  
+  twitter_link;
+
   constructor(private mapService: MapService, private tweetService: TweetService, private router: Router) {
     const state: RouterState = router.routerState;
     const root: ActivatedRoute = state.root;
