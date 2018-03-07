b6331a28fd07f5b1a6f67ef1f6f19d485d457f74
vladimir.tarasov <vladimir.tarasov@eosda.com>
2018-03-07 11:29:12 +0200

    add ctor to register class

--- src/app/register.ts
+++ src/app/register.ts
@@ -5,4 +5,15 @@ export class Register {
     password: string;
     email: string;
     username: string;
+    /**
+     *
+     */
+    constructor(fName:string,lName:string,pass:string,repeatpass:string,mail:string,uName:string) {    
+        this.first_name=fName;
+        this.last_name=lName;
+        this.repeat_password=repeatpass;
+        this.password=pass;
+        this.email=mail;
+        this.username=uName;
+    }
 }
